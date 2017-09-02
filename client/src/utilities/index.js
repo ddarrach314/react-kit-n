3230ddb332dcad125889c644087340f0fde2f1a6
Cody Unger <codyunger@g.ucla.edu>
2017-09-02 11:50:23 -0700

    Write helper function to get targets from store

---
+++ client/src/utilities/index.js
@@ -0,0 +1,8 @@
+import * as storeUtilities from './store';
+import * as actionsUtilities from './actions';
+import _ from 'lodash';
+
+export const utilities = {
+  store: storeUtilities,
+  actions: actionsUtilities
+}
