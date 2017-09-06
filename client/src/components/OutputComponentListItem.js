d53bb7d0a3ac53357e4f33b4f8e7bb90e54c3df4
Cody Unger <codyunger@g.ucla.edu>
2017-09-05 17:27:47 -0700

    Add editing and mouseover for components

--- client/src/components/OutputComponentListItem.js
+++ client/src/components/OutputComponentListItem.js
@@ -1,13 +1,61 @@
 import React from 'react';
 
 class OutputComponentListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      name: props.name,
+      hover: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePencilClick = this.handlePencilClick.bind(this);
+    this.handleCheckClick = this.handleCheckClick.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({editing: false});
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handlePencilClick() {
+    this.setState({editing: true});
+  }
+
+  handleCheckClick() {
+    this.setState({editing: false});
+  }
+
+  handleMouseEnter() {
+    this.setState({hover: true});
+  }
+
+  handleMouseLeave() {
+    this.setState({hover: false});
+  }
+
   render() {
-    return (
+    return this.state.editing ? (
       <div className="outputComponentListItem">
-        <div>{this.props.name}</div>
-        <i className="material-icons">mode_edit</i>
+        <form className="outputComponentListItemName" onSubmit={this.handleSubmit}>
+          <input className="outputComponentListItemInput" onChange={this.handleChange} value={this.state.name}/>
+        </form>
+        <i className="material-icons" onClick={this.handleCheckClick}>done</i>
+      </div>
+    ) : (
+      <div className="outputComponentListItem" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+        <div className="outputComponentListItemName">{this.state.name}</div>
+        {this.state.hover && <i className="material-icons" onClick={this.handlePencilClick}>mode_edit</i>}
       </div>
     );
+
   }
 }
 
