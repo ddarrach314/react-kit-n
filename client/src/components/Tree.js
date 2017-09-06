3fc34015a792482958d70ac147225faa59a73499
Cody Unger <codyunger@g.ucla.edu>
2017-09-06 11:56:28 -0700

    Change borders

--- client/src/components/Tree.js
+++ client/src/components/Tree.js
@@ -5,7 +5,7 @@ import utilities from '../utilities/index';
 class Tree extends React.Component {
   render() {
     return (
-      <div className="col-md-6 treeCol">
+      <div className="col-md-8 treeCol">
         <h3>App Tree</h3>
         <div className="tree">
           {utilities.tree.generateTreeArray(this.props.outputComponents)}
