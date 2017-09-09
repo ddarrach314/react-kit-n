25155bc47c95995d1be40ef6a91416446b3a80fb
nealfwilliams <nealfwilliams@gmail.com>
2017-09-09 11:19:14 -0700

    (feat) Add data persistence via local storage

--- client/src/reduxStore.js
+++ client/src/reduxStore.js
@@ -1,6 +1,20 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
+
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const engine = createEngine('redux-kit-n-storage');
+const storageMiddleware = storage.createMiddleware(engine);
+
+const store = createStore(
+  reducer,
+  applyMiddleware(storageMiddleware)
+);
+
+const load = storage.createLoader(engine);
+
+load(store)
+  .then((newState) => console.log('Loaded state:', newState));
 
 export default store;
